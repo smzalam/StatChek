29367d34b407cd8ceb952719165b1827a55655be
smzalam <alam.syedmz@gmail.com>
2023-11-03 01:22:31 -0400

    deleted mongodb db files

--- src/data/database_conn.py
+++
@@ -1,9 +0,0 @@
-from database_class import MongoDB
-from dotenv import load_dotenv, find_dotenv
-import os, pprint
-
-load_dotenv(find_dotenv())
-password = os.environ.get('MONGODB_PW')
-
-mongodb = MongoDB(password)
-mongodb.connect_client()
--- src/data/database_insert.py
+++
@@ -1,7 +0,0 @@
-from database_conn import mongodb
-
-mongodb.connect_db('MapleLeafs')
-mongodb.connect_collection('teamDetails')
-# mongodb.print_states()
-mongodb.close_connection()
-
